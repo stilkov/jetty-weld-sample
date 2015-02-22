7c08dfdbe534838972a07377c5672bf92426bd6c
Stefan Tilkov <stefan.tilkov@innoq.com>
2015-02-22 09:43:55 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,5 @@
+Minimal web app combining CDI (Weld) and Jetty
+
+After spending more time than I care to admit hunting for the right
+combination of Maven, web.xml and Jetty settings, I thought it might
+make sense to put this online somewhere.
---
+++ src/main/java/com/innoq/sample/SampleServlet.java
@@ -0,0 +1,19 @@
+package com.innoq.sample;
+ 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.inject.Inject;
+
+public class SampleServlet extends HttpServlet
+{
+    @Inject SampleBean bean;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println(bean.getMessage());
+    }
+}
